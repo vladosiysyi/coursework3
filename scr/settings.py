45ce0d13ede76af0ade1272a2e4971438b5feb71
vladosiysyi <129270963+vladosiysyi@users.noreply.github.com>
2025-04-10 20:41:33 +0300

    Update settings.py

--- scr/settings.py
+++ scr/settings.py
@@ -1,6 +1,6 @@
 # settings.py
-DB_NAME = "hh_test"  # Имя базы данных
+DB_NAME = "bd_name"  # Имя базы данных
 DB_USER = "postgres"  # Имя пользователя для подключения к БД
-DB_PASSWORD = "gerfytw"  # Пароль для пользователя
+DB_PASSWORD = "passsword"  # Пароль для пользователя
 DB_HOST = "localhost"  # Хост базы данных
 DB_PORT = "5432"  # Порт базы данных
